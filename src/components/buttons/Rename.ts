f61de04c3064ecf84c855c3a82b7fe75f921502e
Deyaa488 <farahdeyaa7@gmail.com>
2026-05-03 15:35:24 +0100

    Update Rename.ts

--- src/components/buttons/Rename.ts
+++ src/components/buttons/Rename.ts
@@ -14,22 +14,22 @@ export default class RenameButton extends Button {
     if (!interaction.guild) return;
     
     const voice = interaction.guild?.members.cache.get(interaction.user.id)?.voice.channel;
-    if (!voice) return interaction.reply({ content: 'You are not in a voice channel.', flags: MessageFlags.Ephemeral });
+    if (!voice) return interaction.reply({ content: '❌ لست في غرفة صوتية.', flags: MessageFlags.Ephemeral });
     
     const room = await getRoom(voice.id);
-    if (!room) return interaction.reply({ content: 'This is not a valid VoiceMaster room.', flags: MessageFlags.Ephemeral });
+    if (!room) return interaction.reply({ content: '❌ هذه ليست غرفة خاصة بالبوت.', flags: MessageFlags.Ephemeral });
     
     if (room.ownerId !== interaction.user.id) {
-      return interaction.reply({ content: 'You are not the owner of this room.', flags: MessageFlags.Ephemeral });
+      return interaction.reply({ content: '❌ لست مالك هذه الغرفة.', flags: MessageFlags.Ephemeral });
     }
 
     const modal = new ModalBuilder()
       .setCustomId('rename_room_modal')
-      .setTitle('تغيير اسم الغرفة');
+      .setTitle('تغيير اسم الغرفة 📝');
 
     const nameInput = new TextInputBuilder()
       .setCustomId('new_room_name')
-      .setLabel('الاسم الجديد')
+      .setLabel('الاسم الجديد للغرفة')
       .setStyle(TextInputStyle.Short)
       .setPlaceholder('اكتب الاسم هنا...')
       .setRequired(true)
